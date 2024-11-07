6bae43c86a2aa0f1574ce3501e33bdbfaaccb2e1
dhruvRj18 <dhruv4218@gmail.com>
2024-11-07 15:39:02 -0500

    feat: add internet gateway

--- Terraform/aws/vpc/vpc.tf
+++ Terraform/aws/vpc/vpc.tf
@@ -19,3 +19,26 @@ resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet_cidr
 }
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "public_route_table_association" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_route_table_association_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
